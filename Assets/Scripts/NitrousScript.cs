9bc1a0f058750a36580c072b144855643cd4340e
Joshua Cheng <cheng.jo@northeastern.edu>
2024-02-20 01:24:40 -0500

    obstacles, nitro

---
+++ Assets/Scripts/NitrousScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NitrousScript : MonoBehaviour
+{
+
+    public float nitrousDuration = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "Player")
+        {
+            Debug.Log("Nitrous");
+            CarController carController = other.GetComponent<CarController>();
+            carController.activateNitrous(nitrousDuration);
+
+            Destroy(gameObject);
+        }
+        
+    }
+}
