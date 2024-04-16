ff8999efa3145e330a948392f6da2854cce2e37a
Matthew Xie <xiematt10@yahoo.com>
2024-04-16 14:46:04 -0400

    reorg

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -30,12 +30,18 @@ public class PauseMenu : MonoBehaviour
         isGamePaused = false;
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void PauseGame() {
         isGamePaused = true;
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void LoadMainMenu() {
